6a01748a60ad5ff34764fb7eb84b91a3dfb8c877
Amanda Migut <amandamigut@gmail.com>
2025-05-03 00:02:36 +0200

    api IA OK

---
+++ taggerAPI/app/constants.py
@@ -0,0 +1,16 @@
+#Logger
+MODEL_OK = "Modelo IA cargado correctamente."
+MODEL_INIT_ERROR = "Error inicializando el modelo IA: "
+MESSAGE_EMPTY = "Texto vacío recibido en la petición."
+TAG_INPUT_ERROR = "Lista de etiquetas vacía o malformada."
+PROCESSING = "Procesando texto"
+INFERENCE_ERROR = "Error durante la inferencia del modelo IA: "
+TAGS_EMPTY = "No se han encontrado etiquetas relevantes para el texto dado."
+
+
+
+#Errors
+MODEL_KO = "No se pudo inicializar el modelo IA."
+MESSAGE_EMPTY_FAILURE = "El campo 'text' no puede estar vacío."
+TAG_INPUT_ERROR_MESSAGE = "El campo 'labels' debe ser una lista no vacía de etiquetas."
+INFERENCE_ERROR_MESSAGE = "Error interno del servicio de IA. Por favor, inténtelo más tarde."
---
+++ taggerAPI/app/service.py
@@ -0,0 +1,63 @@
+from transformers import pipeline
+from models import TagRequest, TagResponse, TagProbability
+import logging
+from fastapi import HTTPException
+import constants
+
+ZERO_SHOT_TASK = "zero-shot-classification"
+ZERO_SHOT_MODEL = "facebook/bart-large-mnli"
+
+logger = logging.getLogger("uvicorn.error") 
+
+
+class ZeroShotClassifierService:
+
+    def __init__(self):
+            
+            try:
+                from transformers import pipeline
+
+                self.model = pipeline(ZERO_SHOT_TASK, model=ZERO_SHOT_MODEL)
+                logger.info(constants.MODEL_OK)
+
+            except Exception as e:
+
+                logger.error(constants.MODEL_INIT_ERROR,e)
+                raise RuntimeError(constants.MODEL_KO)
+
+    def tag_message(self, request: TagRequest) -> TagResponse:
+
+        if not request.text or not request.text.strip():
+            logger.warning(constants.MESSAGE_EMPTY)
+            raise HTTPException(status_code=422, detail=constants.MESSAGE_EMPTY_FAILURE)
+
+        if not request.labels or not isinstance(request.labels, list) or len(request.labels) == 0:
+            logger.warning(constants.TAG_INPUT_ERROR)
+            raise HTTPException(status_code=422, detail=constants.TAG_INPUT_ERROR_MESSAGE)
+        logger.info(constants.PROCESSING)
+
+        try:
+            result = self.model(request.text, request.labels)
+
+        except Exception as e:
+            logger.error(constants.INFERENCE_ERROR,e)
+            raise HTTPException(
+            status_code=500,
+            detail=constants.INFERENCE_ERROR_MESSAGE,
+            )
+
+        threshold = 0.5
+
+        predicted_labels = [label for label, score in zip(result['labels'], result['scores']) if score >= threshold]
+
+        probabilities = [
+            TagProbability(label=label, score=float(score))
+            for label, score in zip(result['labels'], result['scores'])
+        ]
+
+        if predicted_labels[0] == "":
+
+            logger.info(constants.TAGS_EMPTY)
+            raise HTTPException(status_code=204, detail=constants.TAGS_EMPTY)
+
+        return TagResponse(predicted_labels=predicted_labels, probabilities=probabilities)
